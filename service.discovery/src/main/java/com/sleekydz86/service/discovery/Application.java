268acebb99ce0c04420abf3dbcec1189026120d7
이경용 <sleekydz86@icloud.com>
2025-10-10 01:55:38 +0900

    feat: MSA Eureka 의존성 셋팅

--- service.discovery/src/main/java/com/sleekydz86/service/discovery/Application.java
+++ service.discovery/src/main/java/com/sleekydz86/service/discovery/Application.java
@@ -3,7 +3,12 @@ package com.sleekydz86.service.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
 
     public static void main(String[] args) {
