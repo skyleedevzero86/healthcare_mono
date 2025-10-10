5cc1c2a180d40b34722b60b05ca63465b32a0117
이경용 <sleekydz86@icloud.com>
2025-10-10 20:09:24 +0900

    feat: spring config service commit

---
+++ service.config/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.sleekydz86"
+version = "0.0.1-SNAPSHOT"
+description = "service.discovery"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+    implementation ("org.springframework.cloud:spring-cloud-config-server")
+    //cloud bus
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+    implementation ("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation ("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
