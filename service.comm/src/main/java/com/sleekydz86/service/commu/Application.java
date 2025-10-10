a0edbfb4faebf3edc7fa4863297ca7a9c01653d8
이경용 <sleekydz86@icloud.com>
2025-10-10 21:51:40 +0900

    feat: communication commit

---
+++ service.comm/src/main/java/com/sleekydz86/service/commu/Application.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.service.commu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
