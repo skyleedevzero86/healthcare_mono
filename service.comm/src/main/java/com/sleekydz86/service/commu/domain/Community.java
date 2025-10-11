5bf8eab29f99a714649bfbeab24f4e00714d333e
이경용 <sleekydz86@icloud.com>
2025-10-11 14:46:33 +0900

    feat: service.commu 서비스에서  writeboard 메소드 구현

--- service.comm/src/main/java/com/sleekydz86/service/commu/domain/Community.java
+++ service.comm/src/main/java/com/sleekydz86/service/commu/domain/Community.java
@@ -18,8 +18,9 @@ public class Community {
 
     private String userNm;
 
-    @Column(length = 100000000)
-    @Lob // 대용량 데이터
+    //@Column(length = 100000000)
+    //@Lob // 대용량 데이터
+    @Column(length = 100000)
     private String content;
 
     @Temporal(TemporalType.TIMESTAMP) //날짜 시간
@@ -43,10 +44,12 @@ public class Community {
     @Column()
     private String exercise;
 
+    @Column()
+    private String age;
 
     // 내장타입
-    @Enumerated(EnumType.STRING) //enum 을 String으로 저장
-    private DiseaseCategory category;
+    //@Enumerated(EnumType.STRING) //enum 을 String으로 저장
+    //private DiseaseCategory category;
 
     @Column(name = "user_seq")
     String userSeq;
@@ -54,4 +57,10 @@ public class Community {
     //@JoinColumn(name="user_seq") //실제디비엔 user_seq 칼럼명으로 들어감
     //private Usermng user;
     // DB는 오브젝트를 저장 할 수 없다.FK는 자바는 오브젝트를 저장할 수 없다.
+
+    @PrePersist
+    public void beforePersist() {
+        this.regDate = new Date(); // 현재 시간을 설정합니다.
+    }
+    
 }
