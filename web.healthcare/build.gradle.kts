5ac0394a39f9a2f7192b48a8f6147c0bb0161846
이경용 <sleekydz86@icloud.com>
2025-10-11 19:33:44 +0900

    feat: healthcare 프론프 페이지 의존성 및 환경설정 셋팅

--- web.healthcare/build.gradle.kts
+++ web.healthcare/build.gradle.kts
@@ -18,12 +18,81 @@ repositories {
     mavenCentral()
 }
 
+extra["springCloudVersion"] = "2025.0.0"
+
+configurations {
+    all {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-tomcat")
+    implementation("org.apache.tomcat.embed:tomcat-embed-jasper")
+    implementation("javax.servlet:jstl")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.cloud:spring-cloud-starter")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("org.bgee.log4jdbc-log4j2:log4jdbc-log4j2-jdbc4:1.16")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.core:jackson-core")
+    implementation("com.fasterxml.jackson.core:jackson-annotations")
+    implementation("org.json:json:20200518")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.codehaus.janino:janino:3.1.9")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("junit:junit:4.13.2")
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.8.1")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveBaseName.set("web.healthcare")
+    archiveVersion.set("")
+    archiveExtension.set("jar")
+    enabled = false
+}
+
+tasks.bootJar {
+    archiveBaseName.set("web.healthcare")
+    archiveVersion.set("")
+    archiveExtension.set("jar")
+}
+
+tasks.bootRun {
+    jvmArgs = listOf("-Dfile.encoding=UTF-8", "-Dspring.profiles.active=dev")
 }
